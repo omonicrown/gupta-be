e270d15251a4e79ad7aeee1e1612725ee92fc653
omolade samuel <mac@omolades-MacBook-Pro.local>
2023-11-09 19:41:09 +0200

    commit

--- app/Http/Controllers/Api/Links/AddInfoToLinkController.php
+++ app/Http/Controllers/Api/Links/AddInfoToLinkController.php
@@ -40,7 +40,7 @@ class AddInfoToLinkController extends Controller
                 ], 401);
             }
 
-            // DB::beginTransaction();
+            DB::beginTransaction();
             // if (Link::where('user_id', auth()->user()->id)->count() <= 5) {
                 $link = Link::create([
                     'name' => str_replace(' ', '', $request->name),
@@ -61,6 +61,7 @@ class AddInfoToLinkController extends Controller
                     [
                         'phone_number' => $request->phone_number,
                         'message' => $request->message,
+                        'link_id' => $link->id
                     ]
                 );
 
@@ -68,7 +69,7 @@ class AddInfoToLinkController extends Controller
                     'short_url_id' => $shortURL->id
                 ]);
 
-                // DB::commit();
+                DB::commit();
 
                 return response()->json([
                     'status' => true,
