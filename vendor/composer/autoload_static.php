096de79de7d1b563976fade67e4aabbaaf6989ca
omolade samuel <mac@omolades-MacBook-Pro.local>
2023-11-09 18:54:13 +0200

    commit

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -384,8 +384,8 @@ class ComposerStaticInit59bfadf365662cfd32d50ec6373fa8e1
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
