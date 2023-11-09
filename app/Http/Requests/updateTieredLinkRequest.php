e126c28f37e7e70127b7ea60afb1a98626e9a165
omolade samuel <mac@omolades-MacBook-Pro.local>
2023-11-09 12:37:10 +0200

    commit

--- app/Http/Requests/updateTieredLinkRequest.php
+++ app/Http/Requests/updateTieredLinkRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class createTieredLinkRequest extends FormRequest
+class updateTieredLinkRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
