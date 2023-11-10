25d5f0c1cb048ae782213623f37a7dcbbb233ad8
omolade samuel <mac@omolades-MacBook-Pro.local>
2023-11-10 13:19:40 +0200

    commit

--- app/Http/Controllers/Api/Links/GetSingleLinksController.php
+++ app/Http/Controllers/Api/Links/GetSingleLinksController.php
@@ -36,10 +36,10 @@ class GetSingleLinksController extends Controller
                 // ->whereYear('created_at', '2023')
                 ->select(
                     DB::raw("(count(short_url_id)) as total_click"),
-                    DB::raw("(to_char(created_at, 'YYYY-MM')) as month_year")
+                    DB::raw("(to_char(created_at, 'MM-DD')) as month_day")
                 )
                 // ->groupBy('created_at')
-                ->groupBy(DB::raw("to_char(created_at, 'YYYY-MM')"))
+                ->groupBy(DB::raw("to_char(created_at, 'MM-DD')"))
                 ->get();
 
             $social_traffic = ShortURLVisit::where('short_url_id', $link->short_url_id)
