e8354a6f402b0e77cedd5698f801f76687ebb87c
omolade samuel <mac@omolades-MacBook-Pro.local>
2023-11-10 13:50:46 +0200

    commit

--- app/Http/Controllers/Api/Links/GetSingleLinksController.php
+++ app/Http/Controllers/Api/Links/GetSingleLinksController.php
@@ -35,7 +35,7 @@ class GetSingleLinksController extends Controller
             $visitors = ShortURLVisit::where('short_url_id', $link->short_url_id)
                 // ->whereYear('created_at', '2023')
                 ->select(
-                    DB::raw("(count(short_url_id )) as total_click"),
+                    DB::raw("(count(case when operating_system != 0 then short_url_id end)) as total_click"),
                     DB::raw("(to_char(created_at, 'MM-DD')) as month_day")
                 )
                 // ->groupBy('created_at')
@@ -50,7 +50,7 @@ class GetSingleLinksController extends Controller
                     DB::raw("COUNT(case when operating_system = 'OS X' then operating_system end) as macbook"), 
                     DB::raw("COUNT(case when operating_system = 'AndroidOS' then operating_system end) as android"),
                     DB::raw("COUNT(case when operating_system = 'iOS' then operating_system end) as iphone"),
-                    DB::raw("COUNT(id) as visit"),
+                    DB::raw("COUNT(case when operating_system != 0 then id end) as visit"),
                 )
                 // ->groupBy('id')
                 // ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%M')"))
